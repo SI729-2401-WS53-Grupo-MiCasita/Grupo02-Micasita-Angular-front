fbf31468b0884a8f36a953e0d3362187ba2a0a3c
Myck <maycoljhordan07@gmail.com>
2024-06-04 16:38:35 -0500

    add: new funstion create estates an load iamgesimage

--- src/app/register/components/create-estates/create-estates.component.ts
+++ src/app/register/components/create-estates/create-estates.component.ts
@@ -4,7 +4,7 @@ import { EstatesService } from '../../services/estates-service/estates.service';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
 import { ToastrService } from 'ngx-toastr';
 import {Router, RouterLink} from "@angular/router";
-import {MatCard} from "@angular/material/card";
+import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
 import {NgForOf} from "@angular/common";
 import {MatButton} from "@angular/material/button";
 import {MatRadioButton, MatRadioGroup, MatRadioModule} from "@angular/material/radio";
@@ -13,10 +13,11 @@ import {MatInput} from "@angular/material/input";
 import {MatOption} from "@angular/material/autocomplete";
 import {MatSelect} from "@angular/material/select";
 import {MatStep, MatStepLabel, MatStepper, MatStepperNext, MatStepperPrevious} from "@angular/material/stepper";
+import {StorageService} from "../../services/storage/storage.service";
 
 @Component({
-  selector: 'app-create-estates',
-  standalone: true,
+    selector: 'app-create-estates',
+    standalone: true,
     imports: [
         FormsModule,
         MatCard,
@@ -29,21 +30,22 @@ import {MatStep, MatStepLabel, MatStepper, MatStepperNext, MatStepperPrevious} f
         MatInput,
         MatLabel,
         MatOption,
-        MatSelect, MatRadioModule, MatStepper, MatStep, ReactiveFormsModule, MatStepLabel, MatStepperNext, MatStepperPrevious
+        MatSelect, MatRadioModule, MatStepper, MatStep, ReactiveFormsModule, MatStepLabel, MatStepperNext, MatStepperPrevious, MatCardHeader, MatCardContent
     ],
-  templateUrl: './create-estates.component.html',
-  styleUrl: './create-estates.component.css'
+    templateUrl: './create-estates.component.html',
+    styleUrl: './create-estates.component.css'
 })
 export class CreateEstatesComponent implements OnInit{
-  estate: Estate = new Estate();
-  years: number[] = [];
+    estate: Estate = new Estate();
+    years: number[] = [];
+    imageSrc: string = '';
 
     personalDataForm: FormGroup;
     titleOperationTypeForm: FormGroup;
     locationForm: FormGroup;
     featuresForm: FormGroup;
 
-    constructor(private estatesService: EstatesService, private router: Router, private toastr: ToastrService, private formBuilder: FormBuilder) {
+    constructor(private estatesService: EstatesService, private router: Router, private toastr: ToastrService, private formBuilder: FormBuilder, private storageService: StorageService) {
         // Initialize the form groups
         this.personalDataForm = this.formBuilder.group({
             owner: ['', Validators.required]
@@ -72,21 +74,50 @@ export class CreateEstatesComponent implements OnInit{
         });
     }
 
+    onFileSelected(event: any) {
+        if (event.target.files && event.target.files[0]) {
+            const file = event.target.files[0];
+
+            const reader = new FileReader();
+            reader.onload = e => this.imageSrc = reader.result as string;
+
+            reader.readAsDataURL(file);
+        }
+    }
+
+    onClickAddFiles(id:string ) {
+        const fileInput = document.getElementById(id);
+        if (fileInput) {
+            fileInput.click();
+        }
+    }
+
     onSubmit(): void {
         // Combine the values from all form groups into a single object
         this.estate = {...this.personalDataForm.value, ...this.titleOperationTypeForm.value, ...this.locationForm.value, ...this.featuresForm.value};
-
-        this.estatesService.createEstate(this.estate).subscribe();
-        this.toastr.success('Tu propiedad se creó satisfactoriamente', 'Propiedad agregada',{
-            timeOut: 3000,
+        this.storageService.uploadImage('estates', this.estate.title + "_" + this.estate.id, this.estate.thumbnail).then((urlImage: string | null) => {
+            if (urlImage) {
+                console.log("Url de la imagen : ", urlImage);
+                this.estate.thumbnail = urlImage;
+            } else {
+                this.estate.thumbnail = 'gs://micasita2024-01.appspot.com';
+            }
+            this.estatesService.createEstate(this.estate).subscribe();
+            this.toastr.success('Tu propiedad se creó satisfactoriamente', 'Propiedad agregada', {
+                timeOut: 3000,
+            });
+            this.router.navigate(['/estates']);
         });
-        this.router.navigate(['/estates']);
     }
-  ngOnInit(): void {
-    const currentYear = new Date().getFullYear();
-    for (let year = 2000; year <= currentYear; year++) {
-      this.years.push(year);
+    ngOnInit(): void {
+        const currentYear = new Date().getFullYear();
+        for (let year = 2000; year <= currentYear; year++) {
+            this.years.push(year);
+        }
+
+        if (!this.estate.thumbnail) {
+            this.estate.thumbnail = 'default_thumbnail.png';
+        }
     }
-  }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: true,
-  serverBasePath: "https://my-json-server.typicode.com/drkdevv1/db-server-demo"
+  serverBasePath: "https://my-json-server.typicode.com/drkdevv1/db-server-demo",
+  firebaseConfig: {
+    apiKey: "AIzaSyCtZ3rB5QSBtjrOXhlF2LZzUrW_rDv4gg8",
+    authDomain: "micasita2024-01.firebaseapp.com",
+    projectId: "micasita2024-01",
+    storageBucket: "micasita2024-01.appspot.com",
+    messagingSenderId: "311455173144",
+    appId: "1:311455173144:web:d40e5a0f759c832bd2516d"
+  }
 };
