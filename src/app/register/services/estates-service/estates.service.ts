b76cab9462e288fc59e45c1b601e970b73ab236a
Myck <maycoljhordan07@gmail.com>
2024-06-25 13:12:23 -0500

    Feat: Reservation connection Database

--- src/app/register/services/estates-service/estates.service.ts
+++ src/app/register/services/estates-service/estates.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {catchError, Observable, retry} from 'rxjs';
+import { catchError, Observable, retry } from 'rxjs';
 import { Estate } from "../../model/estate-entity/estate.entity";
+import { Reservation } from "../../model/reservation-entity/reservation.entity";
 import { BaseService } from "../../../shared/services/base.service";
 
 @Injectable({
@@ -30,5 +31,9 @@ export class EstatesService extends BaseService<Estate> {
   getAll(): Observable<Estate[]> {
     return this.http.get<Estate[]>(this.resourcePath(), this.httpOptions)
         .pipe(retry(2), catchError(this.handleError));
+  }
+
+  createReservation(reservation: Reservation): Observable<Reservation> {
+    return this.http.post<Reservation>(`${this.baseUrl}/api/v1/reservations`, reservation);
   }
 }
